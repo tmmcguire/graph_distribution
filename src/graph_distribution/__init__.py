dfbead14825c27902791cc9e7e5c76c58161c94b
Tommy M. McGuire <mcguire@crsr.net>
2021-12-31 17:37:01 -0600

    Implement function returning histogram graph
    Implements graph_rolls returning Matplotlib figure.

--- src/graph_distribution/__init__.py
+++ src/graph_distribution/__init__.py
@@ -1,1 +1,55 @@
-from .dice import roll_nd6
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import uniform, norm, lognorm
+
+from . dice import roll_nd6
+
+def _possible_values(rolls):
+    min = np.min(rolls)
+    max = np.max(rolls)
+    return np.array([i for i in range(min, max + 1)])
+
+def _uniform_dist(rolls):
+    min = np.min(rolls)
+    max = np.max(rolls)
+    ud = uniform(loc=min, scale=max-min)
+    x = np.linspace(ud.ppf(0), ud.ppf(1), rolls.size)
+    pdf = uniform.pdf(x, loc=min, scale=max) * rolls.size
+    return x, pdf
+
+def _normal_dist(rolls):
+    mean = np.mean(rolls)
+    sd = np.std(rolls)
+    nd = norm(loc=mean, scale=sd)
+    x = np.linspace(nd.ppf(0.01), nd.ppf(0.99), rolls.size)
+    pdf = nd.pdf(x) * rolls.size
+    return x, pdf
+
+def _lognormal_dist(rolls):
+    scale = np.exp(np.mean(np.log(rolls)))
+    s = np.std(np.log(rolls))
+    nd = lognorm(scale=scale, s=s)
+    x = np.linspace(nd.ppf(0.001), nd.ppf(0.9999), rolls.size)
+    pdf = nd.pdf(x) * rolls.size
+    return x, pdf
+
+def graph_rolls(rolls):
+    bb = np.insert(_possible_values(rolls) + 0.5, 0, [0.0])
+    hist, bins = np.histogram(rolls, bb)
+    # Graph
+    fig, ax = plt.subplots(1,1)
+    ax.set_xlim([np.min(rolls) - 1, np.max(rolls) + 1])
+    ax.set_ylim([0, np.max(hist) * 1.5])
+    # Uniform distribution
+    ux, updf = _uniform_dist(rolls)
+    _ = ax.plot(ux, updf, 'r-', label='Uniform')
+    # Normal Distribution
+    nx, npdf = _normal_dist(rolls)
+    _ = ax.plot(nx, npdf, 'g-', label='Normal')
+    # Lognormal Distribution
+    lx, lpdf = _lognormal_dist(rolls)
+    _ = ax.plot(lx, lpdf, 'b-', label='Lognormal')
+    # Histogram
+    _ = ax.bar(_possible_values(rolls), hist, align='center', width = 0.7, label = 'Rolls')
+    _ = ax.legend(loc='best')
+    return fig
