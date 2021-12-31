d25a5e4c7731021a68895b7326c85f81b1fb56fd
Tommy M. McGuire <mcguire@crsr.net>
2021-12-31 17:36:32 -0600

    Split roll_nd6 into generic s-sided dice

--- src/graph_distribution/dice.py
+++ src/graph_distribution/dice.py
@@ -3,8 +3,22 @@ from numpy.random import default_rng
 # import matplotlib.pyplot as plt
 # from scipy.stats import uniform, norm, lognorm
 
-def roll_nd6(n, rolls):
-    data = default_rng().integers(1, 7, rolls)
+def roll_ndice(n, r, s):
+    """
+    Roll n dice with s sides, summing the results, r times.
+
+    Returns an array of length r containing the values.
+    """
+    data = default_rng().integers(1, s+1, r)
     for _ in range(1, n):
-        data = data + default_rng().integers(1, 7, rolls)
+        data = data + default_rng().integers(1, s+1, r)
     return data
+
+def roll_nd6(n, r):
+    """
+    Roll n 6-sided dice, summing the results, r times.
+
+    Returns an array of length r containing the values.
+    """
+    return roll_ndice(n, r, 6)
+
