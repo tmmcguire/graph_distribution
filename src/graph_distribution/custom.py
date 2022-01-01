a27d2110841b818b79422776004f66ef7c3d92c0
Tommy M. McGuire <mcguire@crsr.net>
2021-12-31 21:47:28 -0600

    Documentation for SCRUD rollers

--- src/graph_distribution/custom.py
+++ src/graph_distribution/custom.py
@@ -2,6 +2,13 @@ import numpy as np
 from .dice import *
 
 def scrud(n, m, s):
+    """
+    Return the result of using SCRUD on n s-sided dice with m s-sided dice.
+
+    SCRUD is Simple Combat Resolution Using Dice. A negative result means
+    the n rolls scores that many excess successes; positive means the m rolls
+    score that many successes.
+    """
     n_rolls = roll_dice(n, s)
     n_rolls.sort()
     m_rolls = roll_dice(m, s)
@@ -15,8 +22,13 @@ def scrud(n, m, s):
     return sum
 
 def roll_scrud(r, n, m, s):
-    data = np.array([
-        scrud(n, m, s)
-        for _ in range(0, r)
-    ])
+    """
+    Return the resuls on r trials of SCRUD comparing n rolls and m rolls
+    of s-sided dice.
+
+    SCRUD is Simple Combat Resolution Using Dice. A negative result means
+    the n rolls scores that many excess successes; positive means the m rolls
+    score that many successes.
+    """
+    data = np.array([ scrud(n, m, s) for _ in range(0, r) ])
     return data
