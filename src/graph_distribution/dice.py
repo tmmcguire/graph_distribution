413c72c8e945bace82c0b07ca57a4c5820efb0ca
Tommy M. McGuire <mcguire@crsr.net>
2021-12-31 20:42:26 -0600

    Add mult_ndice, cascade_ndice, and success_ndice
    Different ways to combine dice rolls.

--- src/graph_distribution/dice.py
+++ src/graph_distribution/dice.py
@@ -31,3 +31,80 @@ def roll_nd6(n, r):
     """
     return roll_ndice(n, r, 6)
 
+###=====================================
+
+### Combine dice by multiplication.
+
+def mult_ndice(n, r, s):
+    """
+    Roll n dice with s sides, multiplying the results, r times.
+
+    Returns an array of length r containing the values.
+    """
+    data = roll_dice(r, s)
+    for _ in range(1, n):
+        data = data * roll_dice(r, s)
+    return data
+
+def mult_nd6(n, r):
+    """
+    Roll n 6-sided dice, multiplying the results, r times.
+
+    Returns an array of length r containing the values.
+    """
+    return mult_ndice(n, r, 6)
+
+###=====================================
+
+### Roll a single dice; if a high-value is rolled, roll again and add
+### that value.
+
+def cascade_dice(r, s):
+    """Roll one s-sided dice r times.
+
+    If the maximum value is rolled, roll again and add the maximum
+    value. Repeat until no more max-values are rolled.
+
+    Returns an array of length r containing the values.
+
+    """
+    data = roll_dice(r, s)
+    for i in range(0, data.size):
+        max = s
+        while data[i] == max:
+            data[i] += roll_dice(1, s)[0]
+            max += s
+    return data
+
+def cascade_ndice(n, r, s):
+    """
+    Make n cascading s-sided dice rolls (see cascade_dice), adding the results, r times.
+
+    Returns an array of length r containing the values.
+    """
+    data = cascade_dice(r, s)
+    for _ in range(1, n):
+        data = data + cascade_dice(r, s)
+    return data
+
+def cascade_nd6(n, r):
+    """
+    Make n cascading 6-sided dice rolls (see cascade_ndice), adding the results, r times.
+
+    Returns an array of length r containing the values.
+    """
+    return cascade_ndice(n, r, 6)
+
+###=====================================
+
+### Successes: roll a dice greater than m
+
+def successes(r, s, min=None):
+    min = min if min is not None else s
+    return (roll_dice(r, s) >= min) * 1
+
+def success_ndice(n, r, s, min=None):
+    data = successes(r, s, min)
+    for _ in range(1, n):
+        data = data + successes(r, s, min)
+    return data
